155b2bbb70568841de30e146b134bbb8bda64ded
Yuqiao Wang <56175499+allenwyq@users.noreply.github.com>
2022-12-20 03:27:44 +0900

    Added new buttons

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,23 +29,78 @@
         private void InitializeComponent()
         {
             this.ButtonObject = new System.Windows.Forms.Button();
+            this.ButtonPythonInterpreter = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ButtonIn = new System.Windows.Forms.Button();
+            this.ButtonOut = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ButtonObject
             // 
-            this.ButtonObject.Location = new System.Drawing.Point(685, 145);
+            this.ButtonObject.BackColor = System.Drawing.Color.YellowGreen;
+            this.ButtonObject.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ButtonObject.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ButtonObject.Location = new System.Drawing.Point(451, 635);
             this.ButtonObject.Name = "ButtonObject";
-            this.ButtonObject.Size = new System.Drawing.Size(329, 209);
+            this.ButtonObject.Size = new System.Drawing.Size(607, 139);
             this.ButtonObject.TabIndex = 0;
-            this.ButtonObject.Text = "Object...";
-            this.ButtonObject.UseVisualStyleBackColor = true;
+            this.ButtonObject.Text = "Begin Object Detection";
+            this.ButtonObject.UseVisualStyleBackColor = false;
             this.ButtonObject.Click += new System.EventHandler(this.ButtonObject_Click_1);
+            // 
+            // ButtonPythonInterpreter
+            // 
+            this.ButtonPythonInterpreter.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ButtonPythonInterpreter.Location = new System.Drawing.Point(44, 67);
+            this.ButtonPythonInterpreter.Name = "ButtonPythonInterpreter";
+            this.ButtonPythonInterpreter.Size = new System.Drawing.Size(656, 187);
+            this.ButtonPythonInterpreter.TabIndex = 1;
+            this.ButtonPythonInterpreter.Text = "Browse for Python Interpreter Location (python.exe)\r\n(Please use python 3.10)";
+            this.ButtonPythonInterpreter.UseVisualStyleBackColor = false;
+            this.ButtonPythonInterpreter.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button1.Location = new System.Drawing.Point(835, 67);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(591, 187);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse for Python Execution file (MMD.py)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // ButtonIn
+            // 
+            this.ButtonIn.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ButtonIn.Location = new System.Drawing.Point(44, 317);
+            this.ButtonIn.Name = "ButtonIn";
+            this.ButtonIn.Size = new System.Drawing.Size(656, 177);
+            this.ButtonIn.TabIndex = 3;
+            this.ButtonIn.Text = "Select the Input path";
+            this.ButtonIn.UseVisualStyleBackColor = false;
+            this.ButtonIn.Click += new System.EventHandler(this.ButtonIn_Click);
+            // 
+            // ButtonOut
+            // 
+            this.ButtonOut.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ButtonOut.Location = new System.Drawing.Point(835, 317);
+            this.ButtonOut.Name = "ButtonOut";
+            this.ButtonOut.Size = new System.Drawing.Size(591, 177);
+            this.ButtonOut.TabIndex = 4;
+            this.ButtonOut.Text = "Select the Output path";
+            this.ButtonOut.UseVisualStyleBackColor = false;
+            this.ButtonOut.Click += new System.EventHandler(this.ButtonOut_Click);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 48F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1299, 707);
+            this.ClientSize = new System.Drawing.Size(1502, 920);
+            this.Controls.Add(this.ButtonOut);
+            this.Controls.Add(this.ButtonIn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ButtonPythonInterpreter);
             this.Controls.Add(this.ButtonObject);
             this.Name = "FormMain";
             this.Text = "ADLTagger Window";
@@ -56,5 +111,9 @@
         #endregion
 
         private Button ButtonObject;
+        private Button ButtonPythonInterpreter;
+        private Button button1;
+        private Button ButtonIn;
+        private Button ButtonOut;
     }
 }
--- FormMain.cs
+++ FormMain.cs
@@ -1,27 +1,126 @@
 using System;
 using System.IO;
 using System.Diagnostics;
+using System.Windows.Forms;
 
 namespace ObjDet
 {
     public partial class FormMain : Form
     {
+        private string PythonInterpreterPath;
+        private string PythonFilePath;
+
+        private string inputfolder;
+        private string outputfolder;
+
+        private string PythonInputPath;
+        private string PythonOutputPath;
+        //private string PythonModelPath;
+
         public FormMain()
         {
             InitializeComponent();
         }
 
+        // Python Interpreter button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Executable files (*.exe)|*.exe|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Save the selected Python interpreter path
+                    PythonInterpreterPath = openFileDialog.FileName;
+                }
+            }
+        }
+
+        // MMD.py file browse button
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "C:\\";
+                openFileDialog.Filter = "Python files (*.py)|*.py|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 1;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    // Save the selected Python file path
+                    PythonFilePath = openFileDialog.FileName;
+                }
+            }
+        }
+
+        // Button to change input path
+        private void ButtonIn_Click(object sender, EventArgs e)
+        {
+
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    inputfolder = folderBrowserDialog.SelectedPath;
+                }
+            }
+
+            // Read the Python file into a string
+            string inputpath = File.ReadAllText(PythonFilePath);
+
+            // Modify the path within the string using string manipulation techniques
+            // For example, you can use the string.Replace method to replace the old path with the new path
+            inputpath = inputpath.Replace("Input\\data50", inputfolder);
+
+            // Write the modified string back to the Python file
+            File.WriteAllText(PythonFilePath, inputpath);
+
+        }
+        
+        
+        // Button to change output path
+        private void ButtonOut_Click(object sender, EventArgs e)
+        {
+
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    outputfolder = folderBrowserDialog.SelectedPath;
+                }
+            }
+
+            // Read the Python file into a string
+            string outputpath = File.ReadAllText(PythonFilePath);
+
+            // Modify the path within the string using string manipulation techniques
+            // For example, you can use the string.Replace method to replace the old path with the new path
+            outputpath = outputpath.Replace("Output", outputfolder);
+
+            // Write the modified string back to the Python file
+            File.WriteAllText(PythonFilePath, outputpath);
+
+        }
+
+
+
+
+        // Run Object Detection button
         private void ButtonObject_Click_1(object sender, EventArgs e)
         {
-            // Set path to Python interpreter and path to Python file
-            string pythonInterpreterPath = @"C:\Users\allen\.conda\envs\mmd\python.exe";
-            string pythonFilePath = @"C:\Users\allen\Desktop\ObjDet\ObjDet\bin\Debug\net7.0-windows\MMD.py";
+            // Set path to Python file
+            //string pythonFilePath = @"C:\Users\allen\Desktop\ObjDet\ObjDet\bin\Debug\net7.0-windows\MMD.py";
 
             // Create a new ProcessStartInfo object with the appropriate properties
             ProcessStartInfo startInfo = new ProcessStartInfo
             {
-                FileName = pythonInterpreterPath,
-                Arguments = pythonFilePath,
+                FileName = PythonInterpreterPath,
+                Arguments = PythonFilePath,
                 UseShellExecute = false,
                 RedirectStandardOutput = true
             };
@@ -33,5 +132,6 @@ namespace ObjDet
             FormObject formObject = new ObjDet.FormObject();
             formObject.ShowDialog();
         }
+
     }
 }
